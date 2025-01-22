066da2b9b8a1564e9ab884742026faa74805f7fa
Wolnei Hellmann Dircksen <wolneihd@gmail.com>
2025-01-21 21:23:44 -0300

    aula 272

--- secao_22_springboot_jpa/course/src/main/java/com/educandoweb/course/entities/User.java
+++ secao_22_springboot_jpa/course/src/main/java/com/educandoweb/course/entities/User.java
@@ -1,12 +1,17 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +26,10 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() { }
 
@@ -72,6 +81,10 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return this.orders;
+    }    
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
@@ -83,7 +96,6 @@ public class User implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hash(id);
-    }
-   
+    }   
 
 }
